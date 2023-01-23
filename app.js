c24b918e87f318dc8d0e36934320e6e84a511d5e
EbsaT <ebsa16teklu@gmail.com>
2023-01-23 10:59:04 +0300

    return body

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post("/create/order", function (req, res) {
 app.post("/", (req, res) => {
   console.log("Request Body Notify Url");
   console.log({ REQ_BODY: req.body });
-  return req.body
+  res.status(201).json({body: req.body})
 });
 // start server
 let serverPort = process.env.PORT | 8000;

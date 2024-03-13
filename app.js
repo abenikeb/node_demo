e0bb4e83d56fa7c57dece5f156270b3ecfbed4f2
abenikeb79 <abenikeb79@gmail.com>
2024-03-14 05:40:15 +0800

    ss

--- app.js
+++ app.js
@@ -71,12 +71,31 @@ app.post("/create/order", async function (req, res) {
     });
 
     // Send the JSON response to the original HTTP request
-    return res.send(resultRaq).status(200);
+    res.json(resultRaq).status(200);
   } catch (error) {
     console.error("Error creating order:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+// app.post("/create/order", async function (req, res) {
+//   try {
+//     const resultRaq = await createOrder.createOrder(req, res);
+
+//     // Broadcast the result to all connected WebSocket clients
+//     wss.clients.forEach((client) => {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(JSON.stringify(resultRaq));
+//       }
+//     });
+
+//     // Send the JSON response to the original HTTP request
+//     return res.send(resultRaq).status(200);
+//   } catch (error) {
+//     console.error("Error creating order:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 // app.post("/create/order", async function (req, res) {
 //   try {
